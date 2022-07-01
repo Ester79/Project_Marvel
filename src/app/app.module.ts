a75f76ff102e56a819dfde59a1b6a8e6cb315bbd
Ester79 <esterramoscuervo@hotmail.com>
2022-07-01 13:27:41 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    DetailsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
