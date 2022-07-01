d00e37771d0eb107164004e02cf2d9e6051ecaf7
Ester79 <esterramoscuervo@hotmail.com>
2022-07-01 13:47:18 +0200

    Add routing directives / Import components to 'app-routng.module.ts'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Views/Components
+import { MainComponent } from './main/main.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { TeamComponent } from './team/team.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'}, // First view to display when load the application
+  {path: 'main', component: MainComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'detail', component: DetailsComponent},
+  {path: 'team', component: TeamComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
